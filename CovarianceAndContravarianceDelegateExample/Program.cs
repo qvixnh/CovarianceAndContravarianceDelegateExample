0b4a44a34c0410c34b18592e8679f75bf54d24f4
qvixnh <nguyenvinh22012002@gmail.com>
2023-07-30 21:29:05 +0700

    contravariance delegate  methods used for logging information

--- CovarianceAndContravarianceDelegateExample/Program.cs
+++ CovarianceAndContravarianceDelegateExample/Program.cs
@@ -6,22 +6,45 @@ namespace CovarianceAndContravarianceDelegateExample
     class Program
     {
         delegate Car CarFactoryDel(int id, string name);
+        delegate void LogICECarDetailsDel(ICECar car);
+        delegate void LogEVCarDetailsDel(EVCar car);
+
         static void Main(string[] args)
         {
             //ICE Car
             CarFactoryDel carFactoryDel = CarFactory.ReturnICECar;
             Car iceCar = carFactoryDel(1,"Audi R8");
-            Console.WriteLine($"Object type: {iceCar.GetType()}");
-            Console.WriteLine($"Car Details: {iceCar.GetCarDetails()}");
             
             //EV Car
             carFactoryDel = CarFactory.ReturnEVCar;
-            Car evCar = carFactoryDel(2, "Tesla Model-3"); 
+            Car evCar = carFactoryDel(2, "Tesla Model-3");
+            LogICECarDetailsDel logICECarDetailsDel = LogCarDetails;
+            logICECarDetailsDel(iceCar as ICECar);
+            LogEVCarDetailsDel logEVCarDetailsDel = LogCarDetails;
+            logEVCarDetailsDel(evCar as EVCar);
             Console.WriteLine();
-            Console.WriteLine($"Object type: {evCar.GetType()}");
-            Console.WriteLine($"Car Details: {evCar.GetCarDetails}");
             Console.ReadKey();
 
+        }
+        static void LogCarDetails(Car car)
+        {
+            if(car is ICECar)
+            {
+                using (StreamWriter sw = new StreamWriter(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "ICEDetails.txt"), true))
+                {
+                    sw.WriteLine($"Object Type: {car.GetType()}");
+                    sw.WriteLine($"Car Details: {car.GetCarDetails()}");
+                }
+            }
+            else if (car is EVCar)
+            {
+                Console.WriteLine($"Object Type: {car.GetType()}");
+                Console.WriteLine($"Car Details: {car.GetCarDetails}");
+            }
+            else
+            {
+                throw new ArgumentException();
+            }
         }
         
     }
