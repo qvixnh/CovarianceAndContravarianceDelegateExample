0c240b0c441964169454dbf7c0d10531b02c98a8
qvixnh <nguyenvinh22012002@gmail.com>
2023-07-30 20:04:19 +0700

    Add project files.

---
+++ CovarianceAndContravarianceDelegateExample/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+namespace CovarianceAndContravarianceDelegateExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+        
+    }
+    //factory pattern 
+    public static class CarFactory
+    {
+        public static ICECar ReturnICECar(int id, string name)
+        {
+            return new ICECar { Id = id, Name = name };
+
+        }
+        public static EVCar ReturnEVCar(int id, string name) { 
+            return new EVCar { Id = id,Name = name };
+        }
+    }
+
+    public abstract class Car
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public virtual string GetCarDetails()
+        {
+            return $"{Id} - {Name}";
+        }
+    }
+    public class ICECar: Car
+    {
+        public override string GetCarDetails()
+        {
+            return $"{base.GetCarDetails()} - Internal Combustion Engine";
+        }
+    }
+    public class EVCar: Car
+    {
+        public override string GetCarDetails()
+        {
+            return $"{base.GetCarDetails()} - Electric";
+        }
+    }
+}
